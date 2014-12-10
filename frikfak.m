93896f7a73c35d7c9fc07e6d6d8ba18c6a70a5a6
Petrus <petrus.jonsson@gmail.com>
2014-12-10 23:08:08 +0100

    hej

--- frikfak.m
+++ frikfak.m
@@ -3,13 +3,15 @@ interval_start = input(prompt);
 prompt = 'End of interval';
 interval_end = input(prompt);
 
-%[0.0001 0.05]
-Re = 10^4;
-while(Re < 10^5)
+rektvekt = [];
+Re = interval_start;
+while(Re < interval_end)
 
-    fzero(@(k)g(k, 10000),[interval_start interval_end])
-    
+   x1 = fzero(@(k)g(k, Re),[0.00001, 0.05]);
+   rektvekt = [rektvekt; x1];
+
     Re = Re+1000;
 end
-
+disp(rektvekt);
+plot(rektvekt);
 %x2 = fzero(@(k)(4*log10(10000*sqrt(k))-0.4 -(1/sqrt(k))),[0.0001 0.05])
