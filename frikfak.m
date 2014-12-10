d59b63f4f5bd4bc04f932e81d7bb73c7fb60f2ce
Petrus <petrus.jonsson@gmail.com>
2014-12-10 23:46:21 +0100

    hej

--- frikfak.m
+++ frikfak.m
@@ -1,17 +1,20 @@
-prompt = 'Start of interval';
+% ta user input(intervallet)
+prompt = 'Start of interval: ';
 interval_start = input(prompt);
-prompt = 'End of interval';
+prompt = 'End of interval: ';
 interval_end = input(prompt);
 
-rektvekt = [];
+results = [];
 Re = interval_start;
 while(Re < interval_end)
 
    x1 = fzero(@(k)g(k, Re),[0.00001, 0.05]);
-   rektvekt = [rektvekt; x1];
+   results = [results; x1];
 
     Re = Re+1000;
 end
-disp(rektvekt);
+
 plot(rektvekt);
-%x2 = fzero(@(k)(4*log10(10000*sqrt(k))-0.4 -(1/sqrt(k))),[0.0001 0.05])
+title('Resultat')   
+ylabel('Friktionsfaktor')
+xlabel('Reynoldstal')
